2ca34f06c6373096501aad35679cb722a146be7d
HamidMahmood <hamid.mahmood@medznmore>
2023-05-01 01:34:56 +0500

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { RouterProvider } from "./router/router-provider";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/mfa-app">
       <RouterProvider />
     </BrowserRouter>
   );
